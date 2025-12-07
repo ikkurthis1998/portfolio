50904a5e73f4b46ab68c5c5b769e78f8f44c9d57
Sreemannarayana Ikkurthi <ikkurthis1998@gmail.com>
2025-12-07 09:37:10 +0530

    Shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 #[cfg(feature = "ssr")]
-#[tokio::main]
-async fn main() {
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
     use axum::Router;
     use leptos::prelude::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
     use portfolio::App;
     use tower_http::services::ServeDir;
 
-    simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
-
-    let conf = get_configuration(None).unwrap();
+    let conf = get_configuration(Some("Cargo.toml")).unwrap();
     let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
 
-    let app = Router::new()
+    let router = Router::new()
         .nest_service("/assets", ServeDir::new("assets"))
         .leptos_routes(&leptos_options, routes, {
             let opts = leptos_options.clone();
@@ -23,9 +20,7 @@ async fn main() {
         .fallback(leptos_axum::file_and_error_handler(shell))
         .with_state(leptos_options);
 
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    log::info!("listening on http://{}", &addr);
-    axum::serve(listener, app.into_make_service()).await.unwrap();
+    Ok(router.into())
 }
 
 #[cfg(feature = "ssr")]
